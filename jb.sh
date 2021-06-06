b1e25b5f85990940d1e08e8a1f0a12a41b83a488
Pasha Pourmand <pasha.pourmand@gmail.com>
2021-06-05 22:13:00 -0700

    add regular expression for getting ticket id

--- jb.sh
+++ jb.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-echo $(git rev-parse --abbrev-ref HEAD)
+
+# replace with your own
+BASE_URL="https://jira.atlassian.com"
+
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+TICKET_ID=$(expr match "${CURRENT_BRANCH}" '[^/]\+/\([A-Za-z]\+-[0-9]\+\).*')
+
+if [ -z "$1" ]
+then
+    echo "${BASE_URL}/${TICKET_ID}"
+    open "${BASE_URL}/${TICKET_ID}"
+else
+    echo "${BASE_URL}/${1}"
+    open "${BASE_URL}/${1}"
+fi
